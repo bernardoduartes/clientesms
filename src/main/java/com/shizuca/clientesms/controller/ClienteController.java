324ae18db9643f82a0b6620ac2cde529969bf2e5
Bernardo Duarte <bernardo.silva@infobase.com.br>
2022-09-05 23:43:48 -0300

    serviço para salvar clientes e busca por cpf

--- src/main/java/com/shizuca/clientesms/controller/ClienteController.java
+++ src/main/java/com/shizuca/clientesms/controller/ClienteController.java
@@ -1,15 +1,50 @@
 package com.shizuca.clientesms.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.shizuca.clientesms.representation.ClienteSaveRequest;
+import com.shizuca.clientesms.service.ClienteService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("clientes")
 public class ClienteController {
+
+    private final ClienteService service;
 
     @GetMapping
     public String get(){
         return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity save(@RequestBody ClienteSaveRequest request){
+
+        var cliente = request.toModel();
+
+        service.save(cliente);
+
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(cliente.getCpf())
+                .toUri();
+
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity dadosCliente(@RequestParam("cpf") String cpf){
+        var cliente = service.getByCPF(cpf);
+        if(cliente.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(cliente);
     }
 }
---
+++ src/main/java/com/shizuca/clientesms/representation/ClienteSaveRequest.java
@@ -0,0 +1,15 @@
+package com.shizuca.clientesms.representation;
+
+import com.shizuca.clientesms.domain.Cliente;
+import lombok.Data;
+
+@Data
+public class ClienteSaveRequest {
+    private String cpf;
+    private String nome;
+    private Integer idade;
+
+    public Cliente toModel(){
+        return new Cliente(cpf, nome, idade);
+    }
+}
